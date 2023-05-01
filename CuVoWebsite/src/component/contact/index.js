b1bdd5a8cfa51c7bd2eb86d91d3836161da0345d
naveenmnk25 <97008354+naveenmnk25@users.noreply.github.com>
2023-05-01 22:26:54 +0530

    updated done
    hi

--- CuVoWebsite/src/component/contact/index.js
+++ CuVoWebsite/src/component/contact/index.js
@@ -8,7 +8,7 @@ const Contact = () => {
             <section id="contact">
                 <div className="contact-us-pT-45">
                     <div className="row gx-5">
-                        <div className="col-lg-6 col-md-8 col-8 mb-md-0 mb-lg-0 col-sm-12">
+                        <div className="col-lg-6 col-md-8 col-8 mb-md-0 mb-lg-0 col-sm-6">
                             <div className="contact-des">
                                 <div>
                                     <h1 className="text-dark heading contact-us-text">Contact Us</h1>
@@ -19,7 +19,7 @@ const Contact = () => {
                             </div>
                             <BookDemoForm btnPos="center"/>
                         </div>
-                        <div className="col-lg-6 col-md-4 col-4 mb-md-0 mb-lg-0 col-sm-12">
+                        <div className="col-lg-6 col-md-4 col-4 mb-md-0 mb-lg-0 col-sm-6">
                             <img src="../assets/map-image.jpg" className="img-fluid img-fluid-map-image" alt="" />
                         </div>
                     </div>
